a51d9c0b0a7cdb9b07569c7fb4019efb5e90892a
KurnosovNikita <Kurnosov-nikita@mail.ru>
2018-03-01 01:12:42 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-var arr = [];
-var lastj,p=0;
-var openBrackets=0;
-for(var i=0;i<str.length;i++){
-    for(var j=0;j<bracketsConfig.length;j++)
-        {
-            
-               if(str[i]===bracketsConfig[j][0]){
-                
-                openBrackets++;
-                arr.push(str[i]);
-                lastj = j;
-                
+var checkPassed ;
+    var arr = [];
+    if (str.length % 2 !== 0) {
+       return false;
+    } else {
+        for (var i = 0; i < str.length; i++) {
+            for (var j = 0; j < bracketsConfig.length;j++) {
+                if (str[i] == bracketsConfig[j][0] ) {
+                    if(str[i]==bracketsConfig[j][0] && str[i]==bracketsConfig[j][1] && str[i]==arr[arr.length-1]){
+                        arr.pop();
+                    }else{
+                         arr.push(str[i]);
+                    checkPassed =true;
+                    }
+                }
+                if (str[i] == bracketsConfig[j][1] && checkPassed == false && arr.length>0 ) {
+                    if(bracketsConfig[j][0]==arr[arr.length-1]){
+                          arr.pop();
+                    }else{
+                    }
+                }
+                    if(checkPassed == true){
+                        j++;
+                    }
+                     checkPassed =false;
             }
-            
-            
-            if(str[i]===bracketsConfig[j][1] && openBrackets>0 && arr[arr.length-1]==bracketsConfig[j][0] ){
-                openBrackets--;
-                arr.pop();
-            }else if(str[i]===bracketsConfig[j][1] && openBrackets==0){
-                return false;
-                //return false;
-            }else if(str[i]===bracketsConfig[j][1] && arr[arr.length-1]!==bracketsConfig[j][0]){
-                     return false;
-                     }
-            }
-            
-        
-}
-if(openBrackets===0 && arr.length==0){
-    //return true;
-    return true;
-}else if(openBrackets!==0){
-         return false;
-         }
+        }
+    }
+    if (arr.length === 0) {
+        return true;
+    } else if (arr.length !== 0) {
+        return false;
+    }
   // your solution
 }
